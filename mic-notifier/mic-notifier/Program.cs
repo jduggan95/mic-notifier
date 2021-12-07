a49190ecc69c0c6c2d9356f6ff64daf58ca83197
jduggan95 <chills_7@yahoo.com>
2021-12-06 22:15:59 -0600

    Initial proof of concept

---
+++ mic-notifier/mic-notifier/Program.cs
@@ -0,0 +1,54 @@
+﻿using NAudio.CoreAudioApi;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Linq;
+using NAudio.CoreAudioApi.Interfaces;
+
+namespace mic_notifier
+{
+    internal class Program
+    {
+        public static bool ShouldContinue = true;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            MMDevice device = null;
+            var enumerator = new MMDeviceEnumerator();
+            var originalDevices = enumerator.EnumerateAudioEndPoints(DataFlow.Capture, DeviceState.Active);
+            List<MMDevice> devieds = originalDevices.ToList();
+            Console.WriteLine(originalDevices);
+            MMDevice headset = devieds.FirstOrDefault(d => d.DeviceFriendlyName == "G433 Gaming Headset");
+
+            SessionCollection coll = headset.AudioSessionManager.Sessions;
+            for (int i = 0; i < coll.Count; i++)
+            {
+                var session = coll[i];
+                Console.WriteLine(session);
+                if (session.State == NAudio.CoreAudioApi.Interfaces.AudioSessionState.AudioSessionStateActive)
+                {
+                    Console.WriteLine("It is being used by:");
+                    Console.WriteLine(session.GetSessionIdentifier);
+                }
+            }
+
+            headset.AudioSessionManager.OnSessionCreated += SessionCreate;
+
+
+            while (ShouldContinue)
+            {
+                Thread.Sleep(3000);
+            }
+
+            Console.WriteLine("The end");
+
+        }
+
+        static void SessionCreate(object sender, IAudioSessionControl newSession)
+        {
+            Console.WriteLine("The threshold was reached.");
+            ShouldContinue = false;
+        }
+
+    }
+}
